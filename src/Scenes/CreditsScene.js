40df90f1e3680d9c2448a460053ba56bd4b53098
macnick <nick@macexperts.gr>
2020-05-27 19:31:05 +0300

    Fix Credits scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,50 +8,50 @@ export default class CreditsScene extends Phaser.Scene {
 
   create() {
     this.back = this.add.image(400, 300, 'background');
-    this.creditsText = this.add.text(0, 0, 'Credits', {
-      fontSize: '32px',
-      fill: '#fff',
-    });
-    this.madeByText = this.add.text(
+    this.add.image(400, 100, 'title');
+    const style = { fontSize: '22px', fill: '#fff' };
+
+    this.created = this.add.text(
       0,
-      0,
-      'Created By: Placeholder\n\nNick Haralampopoulos\n\nThe Good Co',
-      { fontSize: '26px', fill: '#fff' }
+      120,
+      'Created By: Nick Haralampopoulos',
+      style
     );
+    this.line1 = this.add.text(0, 0, 'Final Capstone Project', style);
+    this.line2 = this.add.text(0, 0, 'in JavaScipt Curriculum', style);
+    this.credits = this.add.text(0, 0, 'Credits', style);
+    this.phaser = this.add.text(0, 0, 'Phaser 3', style);
+    this.open = this.add.text(0, 0, 'OpenGameArt', style);
+
     this.zone = this.add.zone(
       config.width / 2,
-      config.height / 2,
+      config.height / 3,
       config.width,
       config.height
     );
 
-    Phaser.Display.Align.In.Center(this.creditsText, this.zone);
+    Phaser.Display.Align.In.Center(this.created, this.zone);
+    Phaser.Display.Align.In.Center(this.line1, this.zone);
+    Phaser.Display.Align.In.Center(this.line2, this.zone);
+    Phaser.Display.Align.In.Center(this.credits, this.zone);
+    Phaser.Display.Align.In.Center(this.phaser, this.zone);
+    Phaser.Display.Align.In.Center(this.open, this.zone);
 
-    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
+    this.line1.displayOriginY = -30;
+    this.line2.displayOriginY = -60;
+    this.credits.displayOriginY = -120;
+    this.phaser.displayOriginY = -150;
+    this.open.displayOriginY = -180;
 
-    this.madeByText.setY(1000);
+    const btnStyle =
+      'background: url(assets/ui/button_small.png); width: 490px; height: 77px; border: none; font: 32px Georgia; color: #fff;';
+    const btn = this.add.dom(390, 490, 'button', btnStyle, 'Menu');
+    btn.addListener('click');
 
-    this.creditsTween = this.tweens.add({
-      targets: this.creditsText,
-      y: -100,
-      ease: 'Power1',
-      duration: 3600,
-      delay: 1000,
-      onComplete: function () {
-        this.destroy;
-      },
-    });
-
-    this.madeByTween = this.tweens.add({
-      targets: this.madeByText,
-      y: -300,
-      ease: 'Power1',
-      duration: 9000,
-      delay: 1000,
-      onComplete: function () {
-        this.madeByTween.destroy;
-        this.scene.start('Title');
-      }.bind(this),
+    btn.on('click', (event) => {
+      this.model = this.sys.game.globals.model;
+      this.model.score = 0;
+      this.scene.start('Title');
     });
   }
 }
