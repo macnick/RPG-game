a49151a12ed97c99a1e8634e6551ff9f4c660a1f
macnick <nick@macexperts.gr>
2020-06-01 11:06:09 +0300

    Complete the README

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -23,7 +23,7 @@ export default class PreloaderScene extends Phaser.Scene {
         frameHeight: 32,
       }
     );
-    this.load.spritesheet('andomalius', 'assets/images/andromalius-57x88.png', {
+    this.load.spritesheet('and', 'assets/images/andromalius-57x88.png', {
       frameWidth: 57,
       frameHeight: 88,
     });
